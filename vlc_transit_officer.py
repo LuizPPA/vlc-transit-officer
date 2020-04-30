d5e463297511dafb5597096825485e584d8ed52a
Raul <raulfmansur@hotmail.com>
2020-04-30 19:59:06 -0300

    Client, host and main window creation

--- vlc_transit_officer.py
+++ vlc_transit_officer.py
@@ -1,50 +1,80 @@
 import vlc
 import sys
 import os
-import winreg 
+import winreg
 import socket
 import tkinter
+from tkinter import *
 import tkinter.filedialog
 
+APP_NAME = 'VLC Transit Officer'
+CLIENT_WINDOW_NAME = 'Client'
+HOST_WINDOW_NAME = 'Host'
+main_window = None
 
 def init_win_path():
-    vlc_reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\VideoLAN\VLC')
-    vlc_path =  winreg.QueryValueEx(vlc_reg_key, 'InstallDir')[0]
+    vlc_reg_key = winreg.OpenKey(
+        winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\VideoLAN\VLC')
+    vlc_path = winreg.QueryValueEx(vlc_reg_key, 'InstallDir')[0]
 
     if vlc_path not in sys.path:
         sys.path.append(vlc_path)
 
-def main(): 
+
+def open_host_server():
+    # TODO
+    pass
+
+def open_file_prompt():
+    # TODO
+    # root.withdraw()
+    # root.filename = tkinter.filedialog.askopenfilename(filetypes=(
+    #     ('Video files', '*.mp4'), ('Video files', '*.mov'), ('Video files', '*.mkv')))
+    return None
+
+def client_app():
+    global main_window
+    main_window.destroy()
+    client_window = tkinter.Tk()
+    client_window.title(CLIENT_WINDOW_NAME)
+    client_window.geometry('300x50')
+    client_window.resizable(False, False)
+
+    video_path = open_file_prompt()
+
+    client_window.mainloop()
+
+
+def host_app():
+    global main_window
+    main_window.destroy()
+    host_window = tkinter.Tk()
+    host_window.title(HOST_WINDOW_NAME)
+    host_window.geometry('300x50')
+    host_window.resizable(False, False)
+    
+    video_path = open_file_prompt()
+    
+    open_host_server()
+
+    host_window.mainloop()
+
+def main():
     init_win_path()
 
-    # root = tkinter.Tk()
-    # # root.withdraw()
-    # root.filename = tkinter.filedialog.askopenfilename(filetypes = (('Video files', '*.mp4'), ('Video files', '*.mov'), ('Video files', '*.mkv')))
+    global main_window
+    main_window = tkinter.Tk()
+    main_window.title(APP_NAME)
+    main_window.geometry('300x50')
+    main_window.resizable(False, False)
 
-    # # serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # # serv.bind(('0.0.0.0', 8080))
-    # # serv.listen(5)
-    # p = vlc.MediaPlayer(root.filename)
-    # p.play()
-    # #     conn, addr = serv.accept()
+    btn_client = Button(main_window, text='Client', command=client_app)
+    btn_client.grid(column=0, row=0)
 
-    # while True:
-    #     op = input('Op: ')
-    #     if op == '0':
-    #         # conn.send('0')
-    #         p.stop()
-    #         break
-    #     elif op == '1':
-    #         # conn.send('1')
-    #         if p.is_playing():
-    #             p.pause()
-    #         else:
-    #             p.play()
+    btn_host = Button(main_window, text='Host', command=host_app)
+    btn_host.grid(column=1, row=0)
 
-    # # conn.close()
+    main_window.mainloop()
 
-
-if __name__ == "__main__":
-    main()    
-    
-    
+if __name__ == '__main__':
+    main()
